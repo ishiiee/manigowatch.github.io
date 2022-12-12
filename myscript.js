1204b624f53d3924baca53b7db65f7319111995d
ishiiee <120363868+ishiiee@users.noreply.github.com>
2022-12-12 15:27:19 +0800

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,42 @@
+function display1() {
+  document.getElementById("content").innerHTML =
+    '<object type="text/html" width=100% height=100% data="Fantastic Beasts and Where to Find Them.html"></object>';
+}
+function display2() {
+  document.getElementById("content").innerHTML =
+    '<object type="text/html" width=100% height=100% data="Fantastic Beasts The Crimes of Grindelwald.html"></object>';
+}
+function display3() {
+  document.getElementById("content").innerHTML =
+    '<object type="text/html" width=100% height=100% data="Fantastic Beasts The Secrets of Dumbledore.html"></object>';
+}
+function display4() {
+  document.getElementById("content").innerHTML =
+    '<object type="text/html" width=100% height=100% data="JurassicWorldDominion.html"></object>';
+}
+function display5() {
+  document.getElementById("content").innerHTML =
+    '<object type="text/html" width=100% height=100% data="theadamproject1.html"></object>';
+}
+function display6() {
+  document.getElementById("content").innerHTML =
+    '<object type="text/html" width=100% height=100% data="Doctor Strange in the Multiverse of Madness.html"></object>';
+}
+function display7() {
+  document.getElementById("content").innerHTML =
+    '<object type="text/html" width=100% height=100% data="thegrayman1.html"></object>';
+}
+
+function display8() {
+  document.getElementById("content").innerHTML =
+    '<object type="text/html" width=100% height=100% data="bullettrain1.html"></object>';
+}
+
+function display9() {
+  document.getElementById("content").innerHTML =
+    '<object type="text/html" width=100% height=100% data="nobody1.html"></object>';
+}
+function display10() {
+  document.getElementById("content").innerHTML =
+    '<object type="text/html" width=100% height=100% data="home.html"></object>';
+}
